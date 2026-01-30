30d0966276273c14adab6eeef5c1cf9a1a6a69f6
Ismaeldevs <ismaeldevs@gmail.com>
2026-01-30 12:09:45 -0300

    Segundo commit Frontend

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,7 @@
 // Configuración de variables de entorno
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
-
 export const config = {
-  apiUrl: API_URL,
+  apiUrl: import.meta.env.VITE_API_URL || 'nodear-backend-production.up.railway.app:2998/api',
+  mercadoPagoPublicKey: import.meta.env.VITE_MERCADOPAGO_PUBLIC_KEY || 'APP_USR-da978436-2866-4ccd-a04e-6c1beec9d3ac',
   tokenKey: 'nodear_token',
   userKey: 'nodear_user',
 };
